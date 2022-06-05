671fd61c0dd29b748e50905c234e8256fc5cbc7a
edo979 <prezentabilan@gmail.com>
2022-06-05 17:36:40 +0200

    add Alpine.js, basic store and template

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('alpine:init', () => {
+  Alpine.store('commentsAppData', [])
+})
+
+async function getData() {
+  // Get data from Local storage
+  const dataFromLS = JSON.parse(localStorage.getItem('commentsAppData'))
+
+  // Or load form file
+  if (!dataFromLS) {
+    const res = await fetch('data.json'),
+      dataFromFile = await res.json()
+
+    return dataFromFile
+  }
+
+  return dataFromLS
+}
